c5a543ffb508ae25d18945b97d15c6e97397a4c5
rumacalin <russell.macalintal@gmail.com>
2022-02-07 14:08:41 -0500

    Finished implementing Allocator class

--- allocator.js
+++ allocator.js
@@ -4,10 +4,42 @@
 class Allocator {
 
     constructor(max_value){
-        this.current_id = 0;
+        this.current_id = 1;
         this.max_value = max_value;
         this.available_ids = [];
     }
 
+    allocate(){
+        if(this.current_id <= this.max_value){
+            let result = this.current_id;
+            this.current_id += 1;
+            return result;
+        }else if(this.available_ids.length > 0){
+            let result = this.available_ids.shift();
+            return result;
+        }else{
+            console.error('Error: No ID to allocate!')
+        }
+    }
+
+    release(num){
+        if(num < 1 || num > max_value || this.available_ids.includes(num)){
+            console.error('Cannot release ID');
+        }else{
+            this.available_ids.push(num);
+            console.log('ID released!');
+            return this.available_ids;
+        }
+    }
 
 }
+
+let a = new Allocator(10);
+console.log(`Max Value of Allocator: ${a.max_value}`);
+console.log(`Latest available id: ${a.current_id}`);
+
+for(let i = 1; i <= 10; i++){
+    console.log(`ID Allocated: ${a.allocate()}`);
+}
+
+
