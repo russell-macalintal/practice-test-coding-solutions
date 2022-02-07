d1530f4464f6ce4443e3a3b4e2905c1be36b2ed9
rumacalin <russell.macalintal@gmail.com>
2022-02-07 13:41:58 -0500

    Remove console.js for REPL utility

--- allocator.js
+++ allocator.js
@@ -2,6 +2,7 @@
 // Create Class for ID allocator that can allocate and release IDs
 
 class Allocator {
+
     constructor(max_value){
         this.current_id = 0;
         this.max_value = max_value;
